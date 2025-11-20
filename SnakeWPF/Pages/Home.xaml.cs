9f79e7171d98e6350001ae5d7fc9b03c4ea5ba82
qwelord <secondofall22@rambler.ru>
2025-11-20 15:18:07 +0500

    реализовано определение страницы гейм пикулев

--- SnakeWPF/Pages/Home.xaml.cs
+++ SnakeWPF/Pages/Home.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace SnakeWPF.Pages
 {
@@ -23,6 +25,34 @@ namespace SnakeWPF.Pages
         public Home()
         {
             InitializeComponent();
+        }
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.mainWindow.receivingUdpClient != null)
+            {
+                MainWindow.mainWindow.receivingUdpClient.Close();
+            }
+            if (MainWindow.mainWindow.tRec != null)
+            {
+                MainWindow.mainWindow.tRec.Abort();
+            }
+            IPAddress UserIpAdress;
+            if (!IPAddress.TryParse(ip.Text, out UserIpAdress))
+            {
+                MessageBox.Show("Please use the IP adress in the format x.x.x.x.");
+                return;
+            }
+            int UserPort;
+            if (!int.TryParse(port.Text, out UserPort))
+            {
+                MessageBox.Show("Please use the port is a number");
+                return;
+            }
+            MainWindow.mainWindow.StartReceiver();
+            MainWindow.mainWindow.ViewModelUserSettings.IPAddress = ip.Text;
+            MainWindow.mainWindow.ViewModelUserSettings.Port = port.Text;
+            MainWindow.mainWindow.ViewModelUserSettings.Name = name.Text;
+            MainWindow.Send("/start|" + JsonConvert.SerializeObject(MainWindow.mainWindow.ViewModelUserSettings));
         }
     }
 }
