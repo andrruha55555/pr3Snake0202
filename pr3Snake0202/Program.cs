a48fa578a32bb2d2d99a8f94193176f9f8e7291e
student-a502 <student-a502@STUD-RM15-A502>
2025-11-20 14:29:28 +0500

    Прописан метод send самков

--- pr3Snake0202/Program.cs
+++ pr3Snake0202/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using Commonn;
 
 namespace pr3Snake0202
 {
@@ -14,6 +17,37 @@ namespace pr3Snake0202
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5001;
         public static int MaxSpeed = 15;
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    var playerData = viewModelGames.Find(x => x.IdSnake == User.IdSnake);
+                    var otherPlayersData = viewModelGames.FindAll(x => x.IdSnake != User.IdSnake);
+                    var gameData = new GameData
+                    {
+                        PlayerData = playerData,
+                        OtherPlayersData = otherPlayersData
+                    };
+                    byte[] gameDataBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(gameData));
+                    sender.Send(gameDataBytes, gameDataBytes.Length, endPoint);
+                    Console.WriteLine($"Отправил данные игроку {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Возникло исключение" + ex.ToString() + "\n" + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
+        }
         static void Main(string[] args)
         {
         }
